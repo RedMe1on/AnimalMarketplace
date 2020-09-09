61ac6da696355b1c28aa294811e1ecf480eb0b6e
RedMe1on <lalkogad@gmail.com>
2020-09-09 15:07:08 +0300

    fix rating bug
    *fix bug with empty user rating and empty avg rating

--- AnimalsMarketplace/apps/catalogs/utils.py
+++ AnimalsMarketplace/apps/catalogs/utils.py
@@ -1,7 +1,6 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, ObjectDoesNotExist
 
 from .models import Product, Categories, RatingProduct
-
 
 class ProductFilterMixin:
 
@@ -36,17 +35,23 @@ class RatingProductMixin:
     def get_user_rating(self, request, slug):
         """Плучение ранее оставленного рейтинга по ip пользователя"""
         ip = self.get_client_ip(request)
-        user_rating = self.rating_model.objects.get(product=self.model.objects.get(slug=slug), ip=ip)
-        return user_rating.rating
+        try:
+            user_rating = self.rating_model.objects.get(product=self.model.objects.get(slug=slug), ip=ip)
+            return user_rating.rating
+        except ObjectDoesNotExist:
+            return 0
 
     def get_avg_rating(self, slug: str) -> float:
         """Подсчет среднего рейтинга товара"""
         rating = self.rating_model.objects.filter(product=self.model.objects.get(slug=slug))
         star_list = [star.rating for star in rating]
-        sum = 0
-        for star in star_list:
-            sum += star
-        return round(sum / len(star_list), 1)
+        if star_list:
+            sum = 0
+            for star in star_list:
+                sum += star
+            return round(sum / len(star_list), 1)
+        else:
+            return 0
 
     @staticmethod
     def get_client_ip(request):
