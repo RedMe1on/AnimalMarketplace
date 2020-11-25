97c13ffbb346e3e5499fdf0484e36a4142d7859a
RedMe1on <lalkogad@gmail.com>
2020-11-25 13:39:31 +0300

    Test deploy with settings Databses

--- AnimalsMarketplace/settings.py
+++ AnimalsMarketplace/settings.py
@@ -102,14 +102,14 @@ WSGI_APPLICATION = 'AnimalsMarketplace.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.environ['DB_NAME'],
-    #     'USER': os.environ['DB_USER'],
-    #     'PASSWORD': os.environ['DB_PASSWORD'],
-    #     'HOST': os.environ['DB_HOST'],
-    #     'PORT': os.environ['DB_PORT'],
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
