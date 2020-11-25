c0fe0d59cabb9c24d6ced794104ef0d779126c2f
RedMe1on <lalkogad@gmail.com>
2020-11-25 18:15:33 +0300

    Change settings.py

--- AnimalsMarketplace/settings.py
+++ AnimalsMarketplace/settings.py
@@ -166,10 +166,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 
 
 MEDIA_URL = '/media/'
