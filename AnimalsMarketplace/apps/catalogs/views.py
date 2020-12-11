4d16b80153afd8c0cf6bd4fad8bc6ff1ed5f8cf4
RedMe1on <lalkogad@gmail.com>
2020-12-11 17:31:30 +0300

    Start re-design

--- AnimalsMarketplace/apps/catalogs/views.py
+++ AnimalsMarketplace/apps/catalogs/views.py
@@ -18,7 +18,7 @@ class ProductList(ProductFilterMixin, ListView):
 
 
 class MainPage(ProductList):
-    template_name = 'catalogs/main.html'
+    template_name = 'catalogs/main1.html'
 
 
 class CategoriesList(ProductFilterMixin, ListView):
