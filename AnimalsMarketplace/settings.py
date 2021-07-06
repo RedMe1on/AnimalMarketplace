1aeff9ff555ecf42be798af18e47c3020fb7192a
RedMe1on <lalkogad@gmail.com>
2021-07-06 16:37:25 +0300

    create deploy files and settings

--- AnimalsMarketplace/settings.py
+++ AnimalsMarketplace/settings.py
@@ -24,12 +24,13 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j_fo!_kp8l2t*+sb94u9oir$^5glguukgw+*jy4u=k2fk@al%n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'j_fo!_kp8l2t*+sb94u9oir$^5glguukgw+*jy4u=k2fk@al%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -63,7 +64,6 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'silk',
 ]
-
 
 ELASTICSEARCH_DSL = {
     'default': {
@@ -120,13 +120,21 @@ WSGI_APPLICATION = 'AnimalsMarketplace.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'animals_marketplace',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'ENGINE': os.environ.get('SQL_ENGINE', "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        'USER': os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'animals_marketplace',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'admin',
+    #     'HOST': 'localhost',
+    #     'PORT': '5433',
+    # }
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
